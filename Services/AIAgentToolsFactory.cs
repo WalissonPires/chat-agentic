7bb6fcc6fe68fd28a90b0888930831c41c66cd17
Walisson Pires R. Miranda <walissonpiresgv@hotmail.com>
2026-03-27 18:16:13 -0300

    Load tools metadata from folder instead appsettings

--- Services/AIAgentToolsFactory.cs
+++ Services/AIAgentToolsFactory.cs
@@ -1,3 +1,5 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using Microsoft.Extensions.AI;
 using ModelContextProtocol.Client;
 
@@ -5,45 +7,110 @@ namespace ChatAgentic.Services
 {
     public class AIAgentToolsFactory
     {
-        private readonly ILoggerFactory _loggerFactory;
-        private readonly Dictionary<string, AgentTool> _tools;
+        private static readonly JsonSerializerOptions serializerOptions;
 
-        public AIAgentToolsFactory(IConfiguration config, ILoggerFactory loggerFactory)
+        static AIAgentToolsFactory()
         {
-            _tools = config.GetSection("Tools").Get<Dictionary<string, AgentTool>>() ?? [];
+            serializerOptions = new JsonSerializerOptions()
+            {
+                PropertyNameCaseInsensitive = true,
+            };
+
+            serializerOptions.Converters.Add(new JsonStringEnumConverter());
+        }
+
+
+        private readonly ILoggerFactory _loggerFactory;
+
+        public AIAgentToolsFactory(ILoggerFactory loggerFactory)
+        {
             _loggerFactory = loggerFactory;
         }
 
         public async Task<List<AITool>> CreateAsync()
         {
+            var logger = _loggerFactory.CreateLogger<AIAgentToolsFactory>();
+
+            logger.LogDebug("Loading agent tools");
+
             var tools = new List<AITool>();
             var appToolsPath = Path.Combine(Directory.GetCurrentDirectory(), ".agent", "tools");
 
-            foreach (var (key, value) in _tools.Where(x => x.Value.Enabled).ToArray())
+            if (!Directory.Exists(appToolsPath))
             {
-                IClientTransport transport = value.Type switch
+                logger.LogDebug("Tools path not exists: {path}", appToolsPath);
+                return [];
+            }
+
+            var toolsFolders = Directory.GetDirectories(appToolsPath);
+            logger.LogDebug("{count} tools folders found", toolsFolders.Length);
+
+            foreach(var toolFolder in toolsFolders)
+            {
+                logger.LogDebug("Process tool {path}", toolFolder);
+
+                var toolJsonFilename = Path.Combine(toolFolder, "TOOL.json");
+                if (!File.Exists(toolJsonFilename))
                 {
-                    AgentToolType.Stdio => new StdioClientTransport(new StdioClientTransportOptions
+                    logger.LogDebug("Tool file not found: {filename}", toolJsonFilename);
+                    continue;
+                }
+
+                var toolJson = await File.ReadAllTextAsync(toolJsonFilename);
+                if (string.IsNullOrEmpty(toolJson))
+                {
+                    logger.LogDebug("Tool file is empty: {filename}", toolJsonFilename);
+                    continue;
+                }
+
+                var toolMeta = JsonSerializer.Deserialize<AgentTool>(toolJson, serializerOptions);
+                if (toolMeta == null)
+                {
+                    logger.LogDebug("Tool file is invalid: {filename}", toolJsonFilename);
+                    continue;
+                }
+
+                if (!toolMeta.Enabled)
+                {
+                    logger.LogDebug("Tool is inactive");
+                    continue;
+                }
+
+                var toolName = Path.GetFileName(toolFolder);
+                if (string.IsNullOrEmpty(toolName))
+                {
+                    logger.LogDebug("Tool name is empty");
+                    continue;
+                }
+
+                 if (!string.IsNullOrEmpty(toolMeta.WorkDir) && toolMeta.WorkDir.Contains(".."))
+                    throw new Exception($"Tool {toolName} has invalid WorkDir");
+
+                IClientTransport transport = toolMeta.Type switch
+                {
+                    AgentToolType.STDIO => new StdioClientTransport(new StdioClientTransportOptions
                     {
-                        Name = key,
-                        Command = value.Command ?? throw new Exception("Tool command is empy"),
-                        WorkingDirectory = Path.Combine(appToolsPath, key, value.WorkDir ?? string.Empty),
-                        Arguments = value.Args,
-                        EnvironmentVariables = value.Envs
+                        Name = toolName,
+                        Command = toolMeta.Command ?? throw new Exception("Tool command is empy"),
+                        WorkingDirectory = Path.Combine(toolFolder, toolMeta.WorkDir ?? string.Empty),
+                        Arguments = toolMeta.Args,
+                        EnvironmentVariables = toolMeta.Envs
                     }, _loggerFactory),
                     AgentToolType.SSE => new HttpClientTransport(new HttpClientTransportOptions
                     {
-                        Name = key,
-                        Endpoint = !string.IsNullOrEmpty(value.Endpoint) ? new Uri(value.Endpoint) : throw new Exception("Tool endpoint is empty"),
-                        AdditionalHeaders = value.Headers
+                        Name = toolName,
+                        Endpoint = !string.IsNullOrEmpty(toolMeta.Endpoint) ? new Uri(toolMeta.Endpoint) : throw new Exception("Tool endpoint is empty"),
+                        AdditionalHeaders = toolMeta.Headers
                     }, _loggerFactory),
-                    _ => throw new NotSupportedException("Tool type invalid: " + value.Type),
+                    _ => throw new NotSupportedException("Tool type invalid: " + toolMeta.Type),
                 };
 
                 var mcpClient = await McpClient.CreateAsync(transport);
                 var mcpTools = await mcpClient.ListToolsAsync();
-                mcpTools = mcpTools.Select(tool => tool.WithName(new AgentToolFunctionName(key, tool.Name).GetFullName())).ToList();
+                mcpTools = mcpTools.Select(tool => tool.WithName(new AgentToolFunctionName(toolName, tool.Name).GetFullName())).ToList();
                 tools.AddRange(mcpTools);
+
+                logger.LogInformation("Tool {name} registred", toolName);
             }
 
             return tools;
@@ -66,7 +133,7 @@ namespace ChatAgentic.Services
 
     public enum AgentToolType
     {
-        Stdio = 1,
+        STDIO = 1,
         SSE = 2
     }
 
